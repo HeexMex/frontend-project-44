f4a6ccb5525413b4cfe4a4699c7ef19896c16325
HeexMex <f.kukla2007@yandex.ru>
2025-05-20 14:20:19 +0300

    commit 6 reform

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,17 +1,23 @@
 import { defineConfig } from "eslint/config";
+import name from '../src/cli.js';
+import readlineSync from 'readline-sync';
 
-export default function getRandomInt(max) {
-    const number = Math.floor(Math.random() * 99 + 1);
-let correctAnswer;
-if (number % 2 === 0) {
-    correctAnswer = 'yes'
-} else {
-    correctAnswer = 'no'
-}
-return [number, correctAnswer]
-}
-    // let number = getRandomInt(99);
-
-
-
-// export { number, correctAnswer }
+    export default function main() {
+        console.log('Answer "yes" if the number is even, otherwise answer "no".')
+        const number = Math.floor(Math.random() * 99 + 1);
+        console.log('Question:'+ number)
+        const answer = readlineSync.question('Your answer: ').toLowerCase().trim()
+        let correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+        if (number % 2 === 0) {
+        correctAnswer = 'yes'
+        } else {
+        correctAnswer = 'no'
+        }
+        if (answer === correctAnswer) {
+            console.log('Correct!');
+            return true;
+        } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`)
+        return false;
+        }
+    }
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,28 +1,8 @@
-#!/usr/bin/env node 
-    // import number from './brain-even.js';
-    // import correctAnswer from './brain-even.js';
-import getRandomInt from './brain-even.js';
-import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!')
 import name from '../src/cli.js';
+import main from '../bin/brain-even.js'
 
-
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
-    function main() {
-        const [number, correctAnswer] = getRandomInt();
-        console.log('Question:'+ number)
-        const answer = readlineSync.question('Your answer: ').toLowerCase().trim()
     
-
-        // const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
-        if (answer === correctAnswer) {
-            console.log('Correct!');
-            return true;
-        } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`)
-        return false;
-        }
-    }
 
 let i = 0;
 while (i <= 2) {
